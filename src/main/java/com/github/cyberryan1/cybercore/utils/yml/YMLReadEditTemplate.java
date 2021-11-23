68f6954a06d74fbf0ae215c7e775daa9e09bec65
CyberRyan <cyberryan934@gmail.com>
2021-11-23 17:35:24 -0600

    Forgot to reload the config after saving it

--- src/main/java/com/github/cyberryan1/cybercore/utils/yml/YMLReadEditTemplate.java
+++ src/main/java/com/github/cyberryan1/cybercore/utils/yml/YMLReadEditTemplate.java
@@ -2,7 +2,10 @@ package com.github.cyberryan1.cybercore.utils.yml;
 
 public class YMLReadEditTemplate extends YMLReadTemplate {
 
-    public void save() { ymlManager.saveConfig(); }
+    public void save() {
+        ymlManager.saveConfig();
+        ymlManager.reloadConfig();
+    }
 
     public void set( String path, Object value ) {
         getConfig().set( path, value );
