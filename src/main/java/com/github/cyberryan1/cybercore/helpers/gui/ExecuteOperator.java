cbbfc2470018809bc57fbf4ae1022897b5617701
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2022-01-12 01:20:05 -0600

    Added javadocs

--- src/main/java/com/github/cyberryan1/cybercore/helpers/gui/ExecuteOperator.java
+++ src/main/java/com/github/cyberryan1/cybercore/helpers/gui/ExecuteOperator.java
@@ -1,5 +1,10 @@
 package com.github.cyberryan1.cybercore.helpers.gui;
 
+/**
+ * This is how the lambda statements are performed in other classes
+ *
+ * @author CyberRyan
+ */
 public interface ExecuteOperator {
 
     void run();
