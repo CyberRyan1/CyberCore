27504edf34c414d0e0c8bfb97426adaa21b9e3b9
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2022-01-12 01:20:22 -0600

    Added javadocs

--- src/main/java/com/github/cyberryan1/cybercore/utils/yml/YMLReadEditTemplate.java
+++ src/main/java/com/github/cyberryan1/cybercore/utils/yml/YMLReadEditTemplate.java
@@ -1,5 +1,10 @@
 package com.github.cyberryan1.cybercore.utils.yml;
 
+/**
+ * Allows one to also edit the YML file
+ * Inherits all methods from the {@link YMLReadTemplate}
+ * @see YMLReadTemplate
+ */
 public class YMLReadEditTemplate extends YMLReadTemplate {
 
     public void save() {
