4c40114978114d4c4d4e7e4a1e963a0d83d489b5
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2022-06-16 23:23:47 -0500

    Added SubcommandStatus enum

---
+++ src/main/java/com/github/cyberryan1/cybercore/helpers/command/SubcommandStatus.java
@@ -0,0 +1,10 @@
+package com.github.cyberryan1.cybercore.helpers.command;
+
+public enum SubcommandStatus {
+    NORMAL,
+    ERROR,
+    NO_PERMISSION,
+    INVALID_ARGS,
+    INVALID_RUNNER,
+    OTHER;
+}
