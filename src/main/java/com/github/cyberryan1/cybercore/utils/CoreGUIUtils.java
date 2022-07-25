d32884e66cd6aed752223ce49c9e3a3d8d356815
fearinq <fearpayments@gmail.com>
2022-07-25 20:20:59 +0700

    added attribute methods

--- src/main/java/com/github/cyberryan1/cybercore/utils/CoreGUIUtils.java
+++ src/main/java/com/github/cyberryan1/cybercore/utils/CoreGUIUtils.java
@@ -1,6 +1,9 @@
 package com.github.cyberryan1.cybercore.utils;
 
 import org.bukkit.Material;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeModifier;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
@@ -43,5 +46,45 @@ public class CoreGUIUtils {
 
     public static ItemStack getBackgroundGlass() {
         return setItemName( new ItemStack( Material.GRAY_STAINED_GLASS_PANE ),  "&7" );
+    }
+
+    public static ItemStack addAttributes(ItemStack item, String attributeName, double amount, Attribute attribute, boolean hideFlags ) {
+        ItemMeta meta = item.getItemMeta();
+        meta.addAttributeModifier(
+                attribute,
+                new AttributeModifier( attributeName, amount, AttributeModifier.Operation.ADD_NUMBER )
+        );
+        if ( hideFlags == true ) {
+            meta.addItemFlags( ItemFlag.HIDE_ATTRIBUTES );
+        }
+        item.setItemMeta( meta );
+        return item;
+    }
+
+    // ! untested
+    public static ItemStack removeAttributes( ItemStack item, String attributeName, double amount, Attribute attribute, boolean hideFlags ) {
+        ItemMeta meta = item.getItemMeta();
+        meta.removeAttributeModifier( attribute, new AttributeModifier( attributeName, amount, AttributeModifier.Operation.ADD_NUMBER ) );
+        if ( hideFlags == true ) {
+            meta.addItemFlags( ItemFlag.HIDE_ATTRIBUTES );
+        }
+        item.setItemMeta( meta );
+        return item;
+    }
+
+    // ! untested
+    public static ItemStack addItemFlags( ItemStack item, ItemFlag ... flags ) {
+        ItemMeta meta = item.getItemMeta();
+        meta.addItemFlags( flags );
+        item.setItemMeta( meta );
+        return item;
+    }
+
+    // ! untested
+    public static ItemStack removeItemFlags( ItemStack item, ItemFlag ... flags ) {
+        ItemMeta meta = item.getItemMeta();
+        meta.removeItemFlags( flags );
+        item.setItemMeta( meta );
+        return item;
     }
 }
