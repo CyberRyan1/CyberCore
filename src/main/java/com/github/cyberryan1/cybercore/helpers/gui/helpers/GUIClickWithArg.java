128d16b7d4c942c795441de872378eeeecbf0e0e
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2022-08-06 11:52:42 -0500

    Created the GUIClickWithArg

---
+++ src/main/java/com/github/cyberryan1/cybercore/helpers/gui/helpers/GUIClickWithArg.java
@@ -0,0 +1,9 @@
+package com.github.cyberryan1.cybercore.helpers.gui.helpers;
+
+import com.github.cyberryan1.cybercore.helpers.gui.GUIItem;
+
+public interface GUIClickWithArg {
+
+    void run( GUIItem item );
+
+}
