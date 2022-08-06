34871c8da021d9254572230d649616aab322b49f
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2022-08-06 12:01:16 -0500

    Added addAttributes(), removeAttributes(), addItemFlags(), and removeItemFlags() methods

--- src/main/java/com/github/cyberryan1/cybercore/utils/CoreItemUtils.java
+++ src/main/java/com/github/cyberryan1/cybercore/utils/CoreItemUtils.java
@@ -1,6 +1,9 @@
 package com.github.cyberryan1.cybercore.utils;
 
 import org.bukkit.Material;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeModifier;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
@@ -70,5 +73,72 @@ public class CoreItemUtils {
      */
     public static ItemStack createItem( Material material, String name ) {
         return setItemName( new ItemStack( material ), name );
+    }
+
+    /**
+     * Adds an attribute to an item
+     * @param item The item to add the attribute to
+     * @param attributeName The name of the attribute to add
+     * @param amount The amount of the attribute to add
+     * @param attribute The {@link Attribute} to add
+     * @param hideFlags Whether to add the {@link ItemFlag#HIDE_ATTRIBUTES} flag to the item
+     * @return The item with the attribute added
+     */
+    public static ItemStack addAttributes( ItemStack item, String attributeName, double amount, Attribute attribute, boolean hideFlags ) {
+        ItemMeta meta = item.getItemMeta();
+        meta.addAttributeModifier(
+                attribute,
+                new AttributeModifier( attributeName, amount, AttributeModifier.Operation.ADD_NUMBER )
+        );
+        if ( hideFlags == true && meta.getItemFlags().contains( ItemFlag.HIDE_ATTRIBUTES ) == false ) {
+            meta.addItemFlags( ItemFlag.HIDE_ATTRIBUTES );
+        }
+        item.setItemMeta( meta );
+        return item;
+    }
+
+    /**
+     * Removes an attribute from an item
+     * @param item The item to remove the attribute from
+     * @param attributeName The name of the attribute to add
+     * @param amount The amount of the attribute to add
+     * @param attribute The {@link Attribute} to remove
+     * @param hideFlags Whether to add the {@link ItemFlag#HIDE_ATTRIBUTES} flag to the item
+     * @return The item with the attribute removed
+     */
+    public static ItemStack removeAttributes( ItemStack item, String attributeName, double amount, Attribute attribute, boolean hideFlags ) {
+        ItemMeta meta = item.getItemMeta();
+        meta.removeAttributeModifier( attribute, new AttributeModifier( attributeName, amount, AttributeModifier.Operation.ADD_NUMBER ) );
+        if ( hideFlags == true && meta.getItemFlags().contains( ItemFlag.HIDE_ATTRIBUTES ) == false ) {
+            meta.addItemFlags( ItemFlag.HIDE_ATTRIBUTES );
+        }
+        item.setItemMeta( meta );
+        return item;
+    }
+
+    /**
+     * Adds a list of item flags to an item
+     * @param item The item to add the flags to
+     * @param flags A list of {@link ItemFlag} to add to the item
+     * @return The item with the flags added
+     */
+    public static ItemStack addItemFlags( ItemStack item, ItemFlag ... flags ) {
+        ItemMeta meta = item.getItemMeta();
+        meta.addItemFlags( flags );
+        item.setItemMeta( meta );
+        return item;
+    }
+
+    /**
+     * Removes a list of item flags from an item
+     * @param item The item to remove the flags from
+     * @param flags A list of {@link ItemFlag} to remove from the item
+     * @return The item with the flags removed
+     */
+    public static ItemStack removeItemFlags( ItemStack item, ItemFlag ... flags ) {
+        ItemMeta meta = item.getItemMeta();
+        meta.removeItemFlags( flags );
+        item.setItemMeta( meta );
+        return item;
     }
 }
