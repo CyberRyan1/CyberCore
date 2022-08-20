761640d8683b8f8233b5380e42c3ff9ecea910a6
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2022-08-20 13:46:19 -0500

    Made ECyberCore into ICyberCore

---
+++ common/src/main/java/com/github/cyberryan1/cybercore/common/ICyberCore.java
@@ -0,0 +1,14 @@
+package com.github.cyberryan1.cybercore.common;
+
+/**
+ * The ICyberCore class is an interface.
+ * It's used to hold utility classes. <p>
+ *
+ * <b>Note:</b> Whatever class extends from this class needs to
+ * add a <code>getPlugin()</code> method and an <code>initialize()</code>
+ * method or something of that nature, depending on the server type. It
+ * also needs to hold all the utility classes for its respective server type.
+ *
+ * @author CyberRyan1
+ */
+public interface ICyberCore {}
