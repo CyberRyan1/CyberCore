cf6f6d9aea3fa229b07aca0a67de3cf57767c004
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2022-08-20 02:39:23 -0500

    Moving color management to a different class

--- common/src/main/java/com/github/cyberryan1/cybercore/common/ECyberCore.java
+++ common/src/main/java/com/github/cyberryan1/cybercore/common/ECyberCore.java
@@ -12,41 +12,7 @@ import com.github.cyberryan1.cybercore.common.models.logger.ICyberLogger;
  */
 public class ECyberCore {
 
-    private static String primaryColor = "&b";
-    private static String secondaryColor = "&7";
     private static ICyberLogger cyberLogger = null;
-
-    /**
-     * Sets the primary color for the plugin.
-     * This is what "&p" within strings will be replaced with.
-     * @param color The primary color <i>(color code, like "&b")</i>
-     */
-    public static void setPrimaryColor( String color ) {
-        primaryColor = color;
-    }
-
-    /**
-     * Sets the secondary color for the plugin.
-     * This is what "&s" within strings will be replaced with.
-     * @param color The secondary color <i>(color code, like "&7")</i>
-     */
-    public static void setSecondaryColor( String color ) {
-        secondaryColor = color;
-    }
-
-    /**
-     * @return The primary color for the plugin <i>(color code, like "&b")</i>
-     */
-    public static String getPrimaryColor() {
-        return primaryColor;
-    }
-
-    /**
-     * @return The secondary color for the plugin <i>(color code, like "&7")</i>
-     */
-    public static String getSecondaryColor() {
-        return secondaryColor;
-    }
 
     /**
      * Sets the {@link ICyberLogger} for the plugin.
