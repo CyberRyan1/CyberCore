92afdcaa888f08aa30ffc3a93a9149f87ef01771
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2022-08-20 14:58:23 -0500

    Renamed to CyberLogUtils, now a static utility class

---
+++ spigot/src/main/java/com/github/cyberryan1/cybercore/spigot/utils/CyberLogUtils.java
@@ -0,0 +1,64 @@
+package com.github.cyberryan1.cybercore.spigot.utils;
+
+import com.github.cyberryan1.cybercore.spigot.CyberCore;
+
+import java.util.logging.Level;
+
+/**
+ * Manages logging within spigot.
+ *
+ * @author CyberRyan1
+ */
+public class CyberLogUtils {
+
+    private static boolean debugLogging = false;
+
+    /**
+     * Logs a debug message, if the {@link #isLoggingDebug()} method is true
+     * @param msg The message to log
+     */
+    public static void logDebug( String msg ) {
+        if ( debugLogging ) {
+            CyberCore.getPlugin().getLogger().log( Level.INFO, "[DEBUG] " + msg );
+        }
+    }
+
+    /**
+     * Logs an info message
+     * @param msg The message to log
+     */
+    public static void logInfo( String msg ) {
+        CyberCore.getPlugin().getLogger().log( Level.INFO, msg );
+    }
+
+    /**
+     * Logs a warning message
+     * @param msg The message to log
+     */
+    public static void logWarn( String msg ) {
+        CyberCore.getPlugin().getLogger().log( Level.WARNING, msg );
+    }
+
+    /**
+     * Logs an error message
+     * @param msg The message to log
+     */
+    public static void logError( String msg ) {
+        CyberCore.getPlugin().getLogger().log( Level.SEVERE, msg );
+    }
+
+    /**
+     * Sets whether debug logs or enabled
+     * @param b True to enable debug logs, false otherwise
+     */
+    public static void setDebugLogging( boolean b ) {
+        debugLogging = b;
+    }
+
+    /**
+     * @return True if debug logs are enabled, false otherwise
+     */
+    public static boolean isLoggingDebug() {
+        return debugLogging;
+    }
+}
