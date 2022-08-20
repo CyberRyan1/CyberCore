e3e91fb1f9314fd1d090d3d7a8fed19607badf4e
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2022-08-20 13:57:43 -0500

    Added plugin variable

--- spigot/src/main/java/com/github/cyberryan1/cybercore/spigot/CyberCore.java
+++ spigot/src/main/java/com/github/cyberryan1/cybercore/spigot/CyberCore.java
@@ -1,4 +1,24 @@
 package com.github.cyberryan1.cybercore.spigot;
 
-public class CyberCore {
+import com.github.cyberryan1.cybercore.common.ICyberCore;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CyberCore implements ICyberCore {
+
+    private static JavaPlugin plugin = null;
+
+    /**
+     * Sets the plugin instance for the CyberCore class.
+     * @param pl The plugin instance
+     */
+    public static void setPlugin( JavaPlugin pl ) {
+        plugin = pl;
+    }
+
+    /**
+     * @return The plugin instance
+     */
+    public static JavaPlugin getPlugin() {
+        return plugin;
+    }
 }
