f6778833ea886f81010304c111a2f37782ab65cb
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2022-08-22 22:08:18 -0500

    Deleted old files

--- src/main/java/com/github/cyberryan1/cybercore/helpers/gui/helpers/GUIClickWithArg.java
+++
@@ -1,9 +0,0 @@
-package com.github.cyberryan1.cybercore.helpers.gui.helpers;
-
-import com.github.cyberryan1.cybercore.helpers.gui.GUIItem;
-
-public interface GUIClickWithArg {
-
-    void run( GUIItem item );
-
-}
