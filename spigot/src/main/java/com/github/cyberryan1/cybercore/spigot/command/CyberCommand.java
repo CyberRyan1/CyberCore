f1d031e5695582c84a0cb048279845fe2e898750
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2022-09-07 22:54:06 -0500

    Fixed issue with arg lengths

--- spigot/src/main/java/com/github/cyberryan1/cybercore/spigot/command/CyberCommand.java
+++ spigot/src/main/java/com/github/cyberryan1/cybercore/spigot/command/CyberCommand.java
@@ -118,7 +118,7 @@ public abstract class CyberCommand extends BaseCommand implements CommandExecuto
             return true;
         }
 
-        if ( args.length <= super.getMinArgLength() ) {
+        if ( args.length < super.getMinArgLength() ) {
             sendUsage( sender );
             return true;
         }
