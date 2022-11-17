8282af28564196ed69c47c8c83acc6322ebbdfd1
CyberRyan <cyberryan934@gmail.com>
2022-11-16 23:38:05 -0600

    Added TODO

--- spigot/src/main/java/com/github/cyberryan1/cybercore/spigot/CyberCore.java
+++ spigot/src/main/java/com/github/cyberryan1/cybercore/spigot/CyberCore.java
@@ -2,6 +2,8 @@ package com.github.cyberryan1.cybercore.spigot;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+// TODO add a way to add cooldowns to commands/subcommands (w/ a way to bypass them as well)
+
 public class CyberCore {
 
     private static JavaPlugin plugin = null;
